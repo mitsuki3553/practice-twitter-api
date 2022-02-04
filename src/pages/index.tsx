63f64d68f592320260b75ac11a7c5b8f145849b2
mitsuki <mitsuki3553@gmail.com>
2022-02-04 21:16:49 +0900

    🚨 lintの修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "src/styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -16,13 +16,16 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <button onClick={async()=>{
-          const a = await fetch("s");
-        }}>ボタン</button>
+        <button
+          onClick={async () => {
+            const a = await fetch("s");
+          }}
+        >
+          ボタン
+        </button>
       </main>
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
