10bc07b45963127125b88d477173c0946961dbfc
mitsuki <mitsuki3553@gmail.com>
2022-02-05 10:54:14 +0900

    ✨ ツイートの取得

---
+++ src/pages/api/getTweets/index.ts
@@ -0,0 +1,59 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import needle from "needle";
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+  const getParam = req.query;
+
+  // const userId = getParam.userId;
+  const userId = getParam.userId;
+
+  const token = process.env.BEARER_TOKEN;
+
+  const endpointUrl =
+    `https://api.twitter.com/2/users/${userId}/tweets`;
+
+  async function getRequest() {
+    const params = {
+      "tweet.fields": "public_metrics",
+    };
+
+    const res = await needle("get", endpointUrl, params, {
+      headers: {
+        "User-Agent": "v2RecentSearchJS",
+        authorization: `Bearer ${token}`,
+      },
+    });
+    
+    if (res.body) {
+      return res.body;
+    } else {
+      throw new Error("Unsuccessful request");
+    }
+  }
+
+  (async () => {
+    try {
+      // Make request
+      const response = await getRequest();
+
+      res.status(200).json(response);
+
+    //   console.dir(response, {
+    //     depth: null,
+    //   });
+    } catch (e) {
+      console.log(e);
+      process.exit(-1);
+    }
+    // process.exit();
+  })();
+}
